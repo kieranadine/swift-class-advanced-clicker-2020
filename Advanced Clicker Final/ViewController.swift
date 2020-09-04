a5dd0a102e3191aa7d77d5cd634faa17562e30cd
kieranadine <kiera.nadine316@gmail.com>
2020-09-04 12:24:39 +0800

    Initial Commit

---
+++ Advanced Clicker Final/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Advanced Clicker Final
+//
+//  Created by Kiera Nadine M Nazri on 4/9/20.
+//  Copyright © 2020 Kiera Nadine M Nazri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
